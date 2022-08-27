fbc75c62136b05f63a2dceacf8edd2fe7f97b0c9
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2022-08-27 10:49:02 -0300

    mais uma parte feita

--- app.py
+++ app.py
@@ -28,5 +28,17 @@ def select_cliente():
 def select_produto():
     return render_template('select_produto.html', title='Produtos')
 
+@app.route("/cliente-detalhe", methods=['GET', 'POST'])
+def detalhe_clinete():
+    return render_template('detalhe_cliente.html', title='Cliente')
+
+@app.route("/produto-detalhe", methods=['GET', 'POST'])
+def detalhe_produto():
+    return render_template('detalhe_produto.html', title='Produto')
+
+@app.route("/carrinho", methods=['GET', 'POST'])
+def carrinho():
+    return render_template('carrinho.html', title='Carrinho')
+
 if __name__ == __name__:
     app.run(host=host, port=port, debug=debug)
